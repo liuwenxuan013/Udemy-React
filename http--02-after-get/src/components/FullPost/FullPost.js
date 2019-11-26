f81e5d235d93d6c07f370502ef3ba42e77123b9c
Wenxuan Liu <wliu122@myseneca.ca>
2019-11-26 02:19:26 -0500

    add global config

--- http--02-after-get/src/components/FullPost/FullPost.js
+++ http--02-after-get/src/components/FullPost/FullPost.js
@@ -15,7 +15,7 @@ class FullPost extends Component
         {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id))
             {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                     .then(response =>
                     {
                         console.log(response);
@@ -28,7 +28,7 @@ class FullPost extends Component
 
     deleteHandler = () =>
     {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.id)
             .then(response =>
             {
                 console.log(response);
--- http--02-after-get/src/index.js
+++ http--02-after-get/src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.interceptors.request.use(request =>
 {
     console.log(request);
     return request;
-}, error =>
+}
+    , error =>
+    {
+        console.log(error);
+        return Promise.reject(error);
+
+    }
+);
+axios.interceptors.request.use(response =>
 {
-    console.log(error);
-    return Promise.reject(error);
-});
+    console.log(response);
+    return response;
+}
+    , error =>
+    {
+        console.log(error);
+        return Promise.reject(error);
+
+    }
+);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
